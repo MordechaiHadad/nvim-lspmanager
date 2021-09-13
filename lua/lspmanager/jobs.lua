ef76f2fc25838a721fe4d0d3eae461b1fc8edd6e
mordechai <mordechai.hadad01@gmail.com>
2021-09-13 19:22:46 +0300

    Refactor: Goodbye plenary welcome jobstart()

--- lua/lspmanager/jobs.lua
+++ lua/lspmanager/jobs.lua
@@ -1,58 +1,43 @@
 local jobs = {}
 
-local job = require("plenary.job")
 local servers = require("lspmanager.servers")
 
 function jobs.installation_job(lsp, path, is_update)
-    job
-        :new({
-            command = "bash",
-            cwd = path,
-            args = { "-c", servers[lsp].install_script() },
-            on_exit = function(_, exitcode)
-                if is_update then
-                    if exitcode == 0 then
-                        print("Successfully updated " .. lsp)
-                    else
-                        print("Failed to update .. " .. lsp)
-                    end
-                else
-                    if exitcode == 0 then
-                        print("Successfully installed " .. lsp)
+    vim.fn.jobstart({"bash", "-c", servers[lsp].install_script()}, {cwd = path, on_exit = function (_, exitcode)
+        if is_update then
+            if exitcode == 0 then
+                print("Successfully updated " .. lsp)
+            else
+                print("Failed to update .. " .. lsp)
+            end
+        else
+            if exitcode == 0 then
+                print("Successfully installed " .. lsp)
 
-                        vim.schedule(function()
-                            lspmanager.setup_servers() -- FIX: Saying cmd is not specified when installing inside the filetype of the server
+                vim.schedule(function()
+                    lspmanager.setup_servers() -- FIX: Saying cmd is not specified when installing inside the filetype of the server
 
-                            if vim.api.nvim_buf_get_name(0) ~= "" then
-                                vim.cmd("bufdo e")
-                            end
-                        end)
-                    else
-                        print("Failed to install " .. lsp)
+                    if vim.api.nvim_buf_get_name(0) ~= "" then
+                        vim.cmd("bufdo e")
                     end
-                end
-            end,
-        })
-        :start()
+                end)
+            else
+                print("Failed to install " .. lsp)
+            end
+        end
+    end})
 end
 
 function jobs.update_job(lsp, path) -- NOTE: might add mass update if viable
-    job
-        :new({
-            command = "bash",
-            cwd = path,
-            args = { "-c", servers[lsp].update_script() }, --TODO: change how to fetch update scripts dont wanna pollute every lsp with their own copy paste of require npm update script
-            on_exit = function(_, exitcode)
-                if exitcode == 69 then
-                    print(lsp .. " is up to date")
-                elseif exitcode == 70 then -- NOTE: this is just for manual for now i think might refactor this soon
-                    jobs.installation_job(lsp, path, true)
-                elseif exitcode == 0 then
-                    print("Successfully updated " .. lsp) --TODO: refactor this else if stuff makes me feel like yanderedev coding
-                end
-            end,
-        })
-        :start()
+    vim.fn.jobstart({"bash", "-c", servers[lsp].update_script()}, {cwd = path, on_exit = function (_, exitcode)
+        if exitcode == 69 then
+            print(lsp .. " is up to date")
+        elseif exitcode == 70 then -- NOTE: this is just for manual for now i think might refactor this soon
+            jobs.installation_job(lsp, path, true)
+        elseif exitcode == 0 then
+            print("Successfully updated " .. lsp) --TODO: refactor this else if stuff makes me feel like yanderedev coding
+        end
+    end})
 end
 
 return jobs
