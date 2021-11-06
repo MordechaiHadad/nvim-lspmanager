525f8e306331feec35fd8e3a0e92ef0e408ff03b
narutoxy <79555780+NarutoXY@users.noreply.github.com>
2021-11-06 22:44:32 +0530

    lspmanager.info.config.setup() -> lspmanager.setup()

--- lua/lspmanager.lua
+++ lua/lspmanager.lua
@@ -9,6 +9,8 @@ local enable_gdscript = false
 
 lspmanager.setup = function(user_configs)
     servers = require("lspmanager.servers").set(user_configs.lsps or {})
+	-- Setting info customisation
+	require("lspmanager.info.config").setup(user_configs.info or {})
     enable_gdscript = user_configs.enable_gdscript or false
     lspmanager.setup_servers(false, nil)
 end
