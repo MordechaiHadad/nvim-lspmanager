defed8c9e81a0df3be05e6c922c047947abb8832
MordechaiHadad <mordechai.hadad01@gmail.com>
2021-12-02 15:24:42 +0200

    fix(issue with configs.lua): still wip

--- lua/lspmanager/utilities.lua
+++ lua/lspmanager/utilities.lua
@@ -17,18 +17,18 @@ end
 
 function utilities.get_config(name)
     -- needed so we can restore the initial state at the end
-    local was_config_set = require("lspconfig/configs")[name]
-    local was_package_loaded = package.loaded["lspconfig/" .. name]
+    local was_config_set = require("lspconfig.configs")[name]
+    local was_package_loaded = package.loaded["lspconfig." .. name]
 
     -- gets or requires config
     local config = require("lspconfig")[name].document_config
 
     -- restore the initial state
     if not was_config_set then
-        require("lspconfig/configs")[name] = nil
+        require("lspconfig.configs")[name] = nil
     end
     if not was_package_loaded then
-        package.loaded["lspconfig/" .. name] = nil
+        package.loaded["lspconfig." .. name] = nil
     end
 
     return vim.deepcopy(config)
